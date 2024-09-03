16b6febab1908ed7acc5c2feee17dd709b4590ea
Dreeeems <114916033+Dreeeems@users.noreply.github.com>
2024-09-03 15:17:07 +0200

    Switch and case

--- Student_Management_System/Main.java
+++ Student_Management_System/Main.java
@@ -1,9 +1,32 @@
 package Student_Management_System;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import  Student_Management_System.Student;
 
 public class Main {
+
+        public static void management(String response){
+            
+            switch (response) {
+                case "1":
+                    
+                    break;
+                case "2":
+                    
+                    break;
+                case "3":
+                    
+                    break;
+                case "4":
+                    
+                    break;
+                default:
+                    break;
+            }
+
+            
+        }
     public static void main(String[] args) {
         Student student1 = new Student("Alica", "alica.doe@gmail.com", 17);
         Student student2 = new Student("John", "John.doe@gmail.com", 21);
@@ -12,6 +35,15 @@ public class Main {
         students.add(student1);
         students.add(student2);
         students.add(student3);
+
+        System.out.println("What do you want to do ?");
+        System.out.println("Check all students : 1");
+        System.out.println("Create student : 2");
+        System.out.println("Delete student : 3");
+        System.out.println("Quit : 4");
+
+        Scanner  scanner  = new Scanner(System.in);
+       int  response = scanner.nextInt();
         for(Student student : students){
             System.out.println(student.getName() + " is " + student.getAge() + " years old.");
         }
