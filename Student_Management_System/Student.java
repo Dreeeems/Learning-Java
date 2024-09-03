180b473fd4ac5b6dbefa440f165c8f87f4d620cc
Dreeeems <114916033+Dreeeems@users.noreply.github.com>
2024-09-03 14:51:32 +0200

    getName , email , age and setName, email and age methods

--- Student_Management_System/Student.java
+++ Student_Management_System/Student.java
@@ -20,4 +20,21 @@ public class Student {
     public String getName(){
         return name;
     }
+    public String getEmail(){
+        return email;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setName(String name){
+        this.name = name;   
+    }
+    public void setEmail(String email){
+        this.email = email;   
+    }
+    public void setAge(int age){
+        this.age = age;   
+    }
 }
