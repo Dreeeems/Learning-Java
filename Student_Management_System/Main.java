a6edd7a4a4936be95c1e627489d8b4c439f4d426
Dreeeems <114916033+Dreeeems@users.noreply.github.com>
2024-09-03 15:08:31 +0200

    Basic display with an ArrayList

--- Student_Management_System/Main.java
+++ Student_Management_System/Main.java
@@ -1,9 +1,20 @@
 package Student_Management_System;
+import java.util.ArrayList;
+
 import  Student_Management_System.Student;
 
 public class Main {
     public static void main(String[] args) {
         Student student1 = new Student("Alica", "alica.doe@gmail.com", 17);
-        System.out.println(student1.getName() + " is " + student1.getAge() + " years old.");
+        Student student2 = new Student("John", "John.doe@gmail.com", 21);
+        Student student3 = new Student("Alex", "alex.doe@gmail.com", 19);
+        ArrayList <Student> students =  new ArrayList<Student>();
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
+        for(Student student : students){
+            System.out.println(student.getName() + " is " + student.getAge() + " years old.");
+        }
+        
     }
 }
